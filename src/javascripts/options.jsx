f608c3e72ff2e5d64124690e0e4afe156fba3975
yinm <yinm.cminor9@gmail.com>
2018-07-20 20:46:26 +0900

    Add option

---
+++ src/javascripts/options.jsx
@@ -0,0 +1,9 @@
+import '../stylesheets/options'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Settings from './components/Settings'
+
+ReactDOM.render(
+  <Settings/>,
+  document.getElementById('container')
+)
