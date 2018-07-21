5df2ff8f201907ae99787902770a1a8b083cc776
yinm <yinm.cminor9@gmail.com>
2018-07-21 09:22:32 +0900

    Add actions

---
+++ src/javascripts/actions.js
@@ -0,0 +1,19 @@
+import CopyToClipboardAction from './actions/CopyToClipboardAction'
+import GoBackAction from './actions/GoBackAction'
+import GoForwardAction from './actions/GoForwardAction'
+import OpenUrlAction from './actions/OpenUrlAction'
+import OpenUrlInNewTabAction from './actions/OpenUrlInNewTabAction'
+import ScrollDownAction from './actions/ScrollDownAction'
+import ScrollUpAction from './actions/ScrollUpAction'
+
+const actions = {
+  CopyToClipboard: CopyToClipboardAction,
+  GoBack: GoBackAction,
+  GoForward: GoForwardAction,
+  OpenUrl: OpenUrlAction,
+  OpenUrlInNewTab: OpenUrlInNewTabAction,
+  ScrollDown: ScrollDownAction,
+  ScrollUp: ScrollUpAction,
+}
+
+export default actions
